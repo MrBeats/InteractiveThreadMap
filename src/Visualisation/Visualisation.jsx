8ac5e4d27927b12a3cf0a5cf96e215de5a7794d6
jamies97 <jamies97@zedat.fu-berlin.de>
2020-02-04 21:32:29 +0100

    Added first map

--- src/Visualisation/Visualisation.jsx
+++ src/Visualisation/Visualisation.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import * as d3 from "d3";
+import * as topojson from "topojson"
+
 
  class Visualisation extends React.Component {
 
@@ -8,8 +10,36 @@ import * as d3 from "d3";
      }
 
      mapTest() {
+         let width = 960,
+             height = 500;
 
+         let projection = d3.geoMercator()
+             .center([0, 5 ])
+             .scale(150)
+             .rotate([-180,0]);
+
+         let svg = d3.select(this.refs.karte).append("svg")
+             .attr("width", width)
+             .attr("height", height);
+
+         let path = d3.geoPath()
+             .projection(projection);
+
+         let g = svg.append("g");
+
+         d3.json("world.json").then(function(topology) {
+
+             g.selectAll("path")
+                 .data(topojson.feature(topology, topology.objects.countries).features)
+                 .enter().append("path")
+                 .attr("d", path)
+                 .attr("stroke", "white")
+                 .attr("stroke-width", "0.25px")
+                 .attr("fill", "grey")
+
+         });
      }
+
      componentDidMount() {
          this.mapTest()
      }
