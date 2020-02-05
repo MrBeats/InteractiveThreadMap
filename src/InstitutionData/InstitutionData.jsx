94f949a99906dbcda6ea2ff1824418c7959075d7
jamies97 <jamies97@zedat.fu-berlin.de>
2020-02-05 02:49:52 +0100

    Read csv

---
+++ src/InstitutionData/InstitutionData.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import instDataCSV from './extracted_institution_data.csv';
+import * as d3 from 'd3';
+
+class InstitutionData extends React.Component {
+    async getData() {
+        let text = await this.fetchCsv()
+        return d3.csvParseRows(text)
+    }
+
+    fetchCsv() {
+        return fetch(instDataCSV).then(function (response) {
+            let reader = response.body.getReader();
+            let decoder = new TextDecoder('utf-8');
+
+            return reader.read().then(function (result) {
+                return decoder.decode(result.value);
+            });
+        });
+    }
+}
+export default InstitutionData
