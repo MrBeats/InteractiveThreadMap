927c9765a55fc0d1f6748ed936a70c3e35221b2d
jamies97 <jamies97@zedat.fu-berlin.de>
2020-02-09 01:34:31 +0100

    Its working, but the circles are behind the map canvas and the performance is crappy

--- src/Visualisation/Visualisation.jsx
+++ src/Visualisation/Visualisation.jsx
@@ -1,41 +1,72 @@
-import React from 'react';
+import React from 'react'
 import * as mapboxgl from "mapbox-gl"
-import InstitutionData from "../InstitutionData/InstitutionData";
+import InstitutionData from "../InstitutionData/InstitutionData"
+import extData from '../InstitutionData/InstitutionData_Ext.csv'
+import * as d3 from "d3"
 
  class Visualisation extends React.Component {
 
-     async mapTest() {
-         function getLL(d) {
-             return new mapboxgl.LngLat(+d.lng, +d.lat)
-         }
-
+     async mapTest(csvData) {
+         //DatenCheck
+         const InstitutionData = csvData.filter(institution => institution.Latitude !== "failed" && institution.Longitude !== "failed")
+         console.log("Daten sind da:")
+         console.log(InstitutionData)
+         //Karte erstellen
          mapboxgl.accessToken = 'pk.eyJ1IjoiZW5qYWxvdCIsImEiOiJjaWhtdmxhNTIwb25zdHBsejk0NGdhODJhIn0.2-F2hS_oTZenAWc0BMf_uw'
          let map = new mapboxgl.Map({
              container: 'map', // container id
              style: 'mapbox://styles/enjalot/cihmvv7kg004v91kn22zjptsc',
-             center: [-10,30],
+             center: [-10, 30],
              zoom: 2,
          })
          map.scrollZoom.enable()
          map.NavigationControl = new mapboxgl.NavigationControl()
+         map.on("viewreset", update)
+         map.on("move", update)
 
-         let x = await new InstitutionData().getData()
-         console.log(x)
-         //let nav = new mapboxgl.NavigationControl();
-         //map.addControl(nav, 'top-left');
+         let nav = new mapboxgl.NavigationControl();
+         map.addControl(nav, 'top-left');
+         let container = map.getCanvasContainer()
 
-         //let container = map.getCanvasContainer()
-
-         /*
-         let svg = d3.select(this.refs.karte).append("svg")
+         //D3 SVG erstellen
+         let svg = d3.select(container)
+             .append("svg")
              .attr('height','800px')
              .attr('width','1400px')
-             .attr('id','D3svg')
-        */
+             .attr('position','absolute')
+
+
+         let circle = svg.selectAll("circle")
+             .data(InstitutionData)
+             .enter()
+             .append("circle")
+             .attr("r", 4)
+             .attr("stroke", "black")
+             .attr("stroke-width", 2)
+             .attr("fill", "red")
+             .attr("opacity", 0.7)
+
+         function update() {
+             circle
+                 .attr("cx", function(d) { return project([d.Longitude,d.Latitude]).x; })
+                 .attr("cy", function(d) { return project([d.Longitude,d.Latitude]).y; })
+         }
+         function project(d) {
+             //console.log("KEKSE")
+             //console.log(d)
+             const lon = parseFloat(d[0].replace(/,/g, '.'));
+             const lat = parseFloat(d[1].replace(/,/g, '.'));
+
+             //console.log(lon)
+             //console.log(lat)
+             return map.project(new mapboxgl.LngLat(lon, lat));
+         }
+
+         update()
      }
 
      componentDidMount() {
-         this.mapTest()
+         d3.dsv(";", extData).then(this.mapTest)
      }
 
      render() {
