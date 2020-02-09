2e1c5b0ed360586a974c3979d00862a860eacba2
jamies97 <jamies97@zedat.fu-berlin.de>
2020-02-09 22:22:06 +0100

    Cleanup and Performance optimisation

--- src/Visualisation/Visualisation.jsx
+++ src/Visualisation/Visualisation.jsx
@@ -1,44 +1,69 @@
 import React from 'react'
 import * as mapboxgl from "mapbox-gl"
-import InstitutionData from "../InstitutionData/InstitutionData"
-import extData from '../InstitutionData/InstitutionData_Ext.csv'
+import extData from './InstitutionData_Ext.csv'
 import * as d3 from "d3"
 import 'mapbox-gl/dist/mapbox-gl.css'
 import './Visualisation.css'
 
  class Visualisation extends React.Component {
 
-     mapTest(csvData) {
-         //DatenCheck
-         const InstitutionData = csvData.filter(institution => institution.Latitude !== "failed" && institution.Longitude !== "failed")
-         console.log("Daten sind da:")
-         console.log(InstitutionData)
-         //Karte erstellen
-         mapboxgl.accessToken = 'pk.eyJ1IjoiZW5qYWxvdCIsImEiOiJjaWhtdmxhNTIwb25zdHBsejk0NGdhODJhIn0.2-F2hS_oTZenAWc0BMf_uw'
-         let map = new mapboxgl.Map({
-             container: 'map', // container id
-             style: 'mapbox://styles/enjalot/cihmvv7kg004v91kn22zjptsc',
-             center: [-10, 30],
-             zoom: 2,
+     constructor (){
+         super()
+         this.createMap = this.createMap.bind(this)
+     }
+
+     componentDidMount() {
+         d3.dsv(";", extData).then(this.createMap)
+     }
+
+     async readInstitutionData() {
+         return await d3.dsv(";", extData).then(function(data){
+             return data.filter(institution => institution.Latitude !== "failed" && institution.Longitude !== "failed")
          })
-         map.scrollZoom.enable()
-         map.NavigationControl = new mapboxgl.NavigationControl()
-         map.on("viewreset", update)
-         map.on("move", update)
+     }
 
-         let nav = new mapboxgl.NavigationControl();
-         map.addControl(nav, 'top-left');
-         let container = map.getCanvasContainer()
+     async createMap() {
+        let InstitutionData = await this.readInstitutionData()
+        console.log(InstitutionData)
+        console.log(this.readInstitutionData())
+        //Karte erstellen
+        mapboxgl.accessToken = 'pk.eyJ1IjoiZW5qYWxvdCIsImEiOiJjaWhtdmxhNTIwb25zdHBsejk0NGdhODJhIn0.2-F2hS_oTZenAWc0BMf_uw'
+        let map = new mapboxgl.Map({
+            container: 'map', // container id
+            style: 'mapbox://styles/enjalot/cihmvv7kg004v91kn22zjptsc',
+            center: [-10, 30],
+            zoom: 2,
+        })
+        map.scrollZoom.enable()
+        map.NavigationControl = new mapboxgl.NavigationControl()
 
-         let d3Data = InstitutionData.filter(function(d){let coord =  project([d.Longitude,d.Latitude]); return ((coord.x <= 1400 && coord.x >= 0) && (coord.y <= 800 && coord.x >= 0))})
-         //D3 SVG erstellen
-         let svg = d3.select(container).append("svg")
+        let nav = new mapboxgl.NavigationControl();
+        map.addControl(nav, 'top-left');
+        let container = map.getCanvasContainer()
 
-         let div = d3.select("body").append("div")
-             .attr("class", "tooltip")
-             .style("opacity", 0);
+        //Create D3 entryPoints
+        let svg = d3.select(container).append("svg")
 
-         let circle = svg.selectAll("circle")
+        let div = d3.select("body").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0)
+
+        map.on("viewreset", () => this.updateInstitutions(InstitutionData,map,svg,div))
+        map.on("move", () => this.updateInstitutions(InstitutionData,map,svg,div))
+
+        console.log(InstitutionData)
+        this.updateInstitutions(InstitutionData,map,svg,div)
+    }
+
+     updateInstitutions(csvData,map,svg,div) {
+         let d3Data = csvData.filter(function(d){let coord =  projectOnMap([d.Longitude,d.Latitude]); return ((coord.x <= 1400 && coord.x >= 0) && (coord.y <= 800 && coord.x >= 0))})
+
+         svg.selectAll("circle")
+             .data(d3Data)
+             .exit()
+             .remove()
+
+         svg.selectAll("circle")
              .data(d3Data)
              .enter()
              .append("circle")
@@ -47,7 +72,6 @@ import './Visualisation.css'
              .attr("stroke-width", 2)
              .attr("fill", "red")
              .attr("opacity", 0.7)
-
              .on("mouseover", function(d) {
                  div.transition()
                      .duration(200)
@@ -62,47 +86,16 @@ import './Visualisation.css'
                      .style("opacity", 0);
              })
 
+         svg.selectAll("circle")
+             .attr("cx", function(d) { return projectOnMap([d.Longitude,d.Latitude]).x; })
+             .attr("cy", function(d) { return projectOnMap([d.Longitude,d.Latitude]).y; })
 
-         function update() {
-             d3Data = InstitutionData.filter(function(d){let coord =  project([d.Longitude,d.Latitude]); return ((coord.x <= 1400 && coord.x >= 0) && (coord.y <= 800 && coord.x >= 0))})
-
-             //console.log(d3Data.length)
-
-             //console.log(circle)
-             //console.log(circle.exit())
-             //console.log(circle.enter())
-
-             circle
-                 .exit().remove()
-             circle
-                 .enter()
-                 .append("circle")
-                 .attr("r", 4)
-                 .attr("stroke", "black")
-                 .attr("stroke-width", 2)
-                 .attr("fill", "red")
-                 .attr("opacity", 0.7)
-             circle
-                 .attr("cx", function(d) { return project([d.Longitude,d.Latitude]).x; })
-                 .attr("cy", function(d) { return project([d.Longitude,d.Latitude]).y; })
-         }
-
-         function project(d) {
-             //console.log("KEKSE")
-             //console.log(d)
+         function projectOnMap(d) {
              const lon = parseFloat(d[0].replace(/,/g, '.'));
              const lat = parseFloat(d[1].replace(/,/g, '.'));
 
-             //console.log(lon)
-             //console.log(lat)
              return map.project(new mapboxgl.LngLat(lon, lat));
          }
-
-         update()
-     }
-
-     componentDidMount() {
-         d3.dsv(";", extData).then(this.mapTest)
      }
 
      render() {
